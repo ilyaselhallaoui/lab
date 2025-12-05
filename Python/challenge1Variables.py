3016b05e68096e63e55e3bd0fd1030e19b9d05cd
ilyas <ilyas.elhallaoui@gmail.com>
2025-12-05 11:55:36 +0100

    my own try of finishing challenge 1 in the Python learning path

---
+++ Python/challenge1Variables.py
@@ -0,0 +1,21 @@
+# 1. Variables / Types
+
+
+
+# Challenge 1 - Variables
+
+# Python has a bunch of pre-made, built-in tools which you can use to perform task.
+# One is these tools is the input() function which allows us to retrieve a user input.
+# You can see these here, including the input() function: https://www.w3schools.com/python/python_ref_functions.asp
+
+# Read about the input() function, try out the example to see how it works, and then only proceed.
+
+# For this challenge, use this input function to ask “What is your favorite color?”.
+# Save the response into a variable named my_input.
+# Then print “Your favorite color is my_input”.
+# So if I type red as my favorite color, it will print “Your favorite color is red”.
+
+# WORK OUT YOUR SOLUTION HERE
+print("What's your favourite color?")
+color=input()
+print("Your favourite color is " + color)
